20efadd65a7c9c0aeb7d12e3d7f1405f4223eb64
Irdgyu <gilangbayuirwana@gmail.com>
2025-06-03 13:41:46 +0700

    Gilang update class kendaraan

--- Kendaraan.java
+++ Kendaraan.java
@@ -1,1 +1,15 @@
+public class Kendaraan {
+    String platNomor, tipe, merk;
 
+    public Kendaraan(String plat, String tipe, String merk) {
+        this.platNomor = plat;
+        this.tipe = tipe;
+        this.merk = merk;
+    }
+
+    public void tampilkanInformasi() {
+        System.out.println("Plat Nomor: " + platNomor);
+        System.out.println("Tipe: " + tipe);
+        System.out.println("Merk: " + merk);
+    }
+}
