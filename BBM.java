d6760b1961ab310abe3234549c99fb98aa4706ed
Sharren <sharrenepf@gmail.com>
2025-06-06 14:37:04 +0700

    commit sharren

--- BBM.java
+++ BBM.java
@@ -1,1 +1,9 @@
+public class BBM {
+    String namaBBM;
+    double hargaPerLiter;
 
+    public BBM(String namaBBM, double harga) {
+        this.namaBBM = namaBBM;
+        hargaPerLiter = harga;
+    }
+}
